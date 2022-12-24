669d7538e078f0fbcd6046a20ced845f862987ae
memoryT <120026359+memoryT@users.noreply.github.com>
2022-12-24 21:08:32 +0800

    Update s_test_2.py

--- s_test_2.py
+++ s_test_2.py
@@ -14,3 +14,9 @@ with open("shopee.json", "r") as f:
 for c in data:
     driver.add_cookie(c)  #將cookie載入
 driver.refresh() #重新整理
+free_url = "https://shopee.tw/m/free-shipping"
+driver.get(free_url)
+
+driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[1]/div/div[2]/div/div[3]/div/div/div[3]/div/div[1]/div/div[1]/div").click()
+
+#嘗試點擊優惠券專區結果報錯，無法點擊後出現免運券領取
